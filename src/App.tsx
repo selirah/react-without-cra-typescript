fde1d298e2ca870ab25d0cbdfcd327d9c2aa29fa
selirah <selirah@pces.mk>
2022-09-15 12:39:15 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import ClickCounter from './ClickCounter'
 
 const App = () => {
+  const num = 0
   return (
     <div>
-      <h1>React Typescript Webpack Starter Starter</h1>
+      <h1>
+        Edited React Typescript Webpack Starter Starter - {process.env.NODE_ENV} {process.env.name}{' '}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCounter />
     </div>
   )
 }
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const ClickCounter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
+    </div>
+  )
+}
+
+export default ClickCounter
