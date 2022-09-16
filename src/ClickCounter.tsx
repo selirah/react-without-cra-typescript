8ab1ddfcbd68e3be3ec15f64c91f316c35ccb5a7
selirah <selirah@pces.mk>
2022-09-16 14:32:12 +0000

    husky error

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 const ClickCounter = () => {
   const [count, setCount] = useState(0)
 
+  const name = ''
+
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
